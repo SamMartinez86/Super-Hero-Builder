ad5d1bceea08a0e048d4bc73d2a48057e5b5b28b
Sam Martinez <73866228+SamMartinez86@users.noreply.github.com>
2021-02-23 17:20:21 -0700

    add login and css pages

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once('./Session/Session.php');
+
+    $session = new session();
+
+    echo $_POST["username"];
+    echo $_POST["password"];
+
+    
+    //$session->login("sam@cs3620sqlsam", "1801Church");
+?>  
